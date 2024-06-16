6907e1b6bb475413c4501f567c61208763dd01a0
11-h-30 <juancarloslevjim@gmail.com>
2024-06-16 06:28:17 +0200

    Modificación ligera del archivo Directory Enumeration.py

--- Directory Enumeration.py
+++ Directory Enumeration.py
@@ -6,20 +6,20 @@ import pyfiglet
 banner = pyfiglet.figlet_format("Russoski Tools \n Directory Enum")
 print(banner)
 
-# Creamos una variable en la que preguntar al usuario por un Dominio
+# Creamos una variable en la que preguntar al usuario por un Dominio (ejemplo: youtube.com)
 dominio = input("-> Introduce un Dominio para enumerar: ")
 print("-> Estamos comprobando todo...")
 
-# La variable Lista se encargará de almancenar y leer el contenido del diccionario
-# La variable Directorios contendrá el contenido de Lista pero dividido en líneas
-lista = open("common.txt").read()
-directorios = lista.splitlines()
+# La variable Archivo se encargará de almancenar y leer el contenido del diccionario
+# La variable Directorios contendrá el contenido de Archivo pero dividido en líneas
+archivo = open("common.txt").read()
+directorios = archivo.splitlines()
 
 # Iniciamos un bucle For que recorrerá cada línea del diccionario en busca de Directorios válidos
-for linea in directorios:
+for directorio in directorios:
     # Realizaremos en cada vuelta una petición web con el Dominio que ingrese
     # el usuario y la línea del diccionario correspondiente a dicha vuelta
-    linea_enumeracion = f"https://{dominio}/{linea}" 
+    linea_enumeracion = f"https://{dominio}/{directorio}" 
     respuesta = requests.get(linea_enumeracion)
     # Si el código de respuesta que arroja la petición es 404 (Not Found), se ignora dicha línea
     if respuesta.status_code==404: 
